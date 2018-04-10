f055afd362275f2dcffbc4c4904d9bc55b3d84a5
bounaykut <aykut___1995@hotmail.com>
2018-04-10 20:29:01 +0300

    other ticket purchase tests are added

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
 var Migrations = artifacts.require("./Migrations.sol");
-var Lottery = artifacts.require("Lottery");
+var Lottery = artifacts.require("./Lottery.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
--- test/lottery.js
+++ test/lottery.js
@@ -1,66 +1,159 @@
 var Lottery = artifacts.require("./Lottery.sol");
 
+
 contract('Lottery', function(accounts){
 
-	var ticketHash = web3.sha3(number1,number2,number3,accounts[0]);
-	var number1 = 1;
-	var number2 = 2;
-	var number3 = 3;
-	var contractAddress = "0xf400c673623a30f8c625095594dae935b4494e80";
-
-	it("exactly 8 finney should be send to buy a full ticket", function(){		
+	it("exactly 8 finney should be send to buy a full ticket", function(){
+		var ticketHash = web3.sha3(number1,number2,number3,accounts[0]);
+		var number1 = "1";
+		var number2 = "2";
+		var number3 = "3";
 		var lotteryBalanceFirst;
-		var lotteryBalanceEnd; 
-		
-		return Promise.resolve(Lottery.at(contractAddress)).then(function(instance){
-						return web3.eth.getBalance(contractAddress);
-					}).then(function(balance){
-						lotteryBalanceFirst = balance;
-						return Lottery.at(contractAddress).buyfullticket.call(ticketHash,{from:accounts[0],value:web3.toWei(8,"finney")});
-					}).then(function(){
-						return web3.eth.getBalance(contractAddress);
-					}).then(function(balance){
-						lotteryBalanceEnd = balance;
-						assert.equal(lotteryBalanceFirst + parseInt(web3.toWei(8,"finney")), lotteryBalanceEnd, "lottery balance should be 8 finney more than the lottery balance before a full ticket sold");							
-					});
+		var lotteryBalanceEnd;
+		var instance;
+
+		return Lottery.deployed().then(function(contract){
+			instance = contract;
+			return contract.getcontractbalance.call();
+		}).then(function(balance){
+			lotteryBalanceFirst = parseInt(balance);
+			return instance.buyfullticket(ticketHash,{from:accounts[0],value:web3.toWei(8,"finney")});
+		}).then(function(result){
+			return instance.getcontractbalance.call();
+		}).then(function(balance){
+			lotteryBalanceEnd = parseInt(balance);
+			assert.equal(lotteryBalanceFirst + parseInt(web3.toWei(8,"finney")),lotteryBalanceEnd,"lottery balance should be 8 finneys more than the lottery balance before a full ticket sell");
+		})
+
 	});
 
+});
 
-	/*it("exactly 4 finney should be send to buy a half ticket", function(){	
-		var lotteryBalanceFirst;
-		var lotteryBalanceEnd; 
-		
-		return Lottery.at(contractAddress).then(function(instance){
-						return web3.eth.getBalance(contractAddress);
-					}).then(function(balance){
-						lotteryBalanceFirst = balance;
-						return Lottery.at(contractAddress).buyfullticket.call(ticketHash,{from:accounts[0],value:web3.toWei(4,"finney")});
-					}).then(function(){
-						return web3.eth.getBalance(contractAddress);
-					}).then(function(balance){
-						lotteryBalanceEnd = balance;
-						assert.equal(lotteryBalanceFirst + parseInt(web3.toWei(4,"finney")), lotteryBalanceEnd, "lottery balance should be 4 finney more than the lottery balance before a half ticket sold");							
-					});
-	});	
 
-	
-	it("exactly 2 finney should be send to buy a quarter ticket", function(){
-		var lotteryBalanceFirst;
-		var lotteryBalanceEnd; 
-		
-		return Lottery.at(contractAddress).then(function(instance){
-						return web3.eth.getBalance(contractAddress);
-					}).then(function(balance){
-						lotteryBalanceFirst = balance;
-						return Lottery.at(contractAddress).buyfullticket.call(ticketHash,{from:accounts[0],value:web3.toWei(2,"finney")});
-					}).then(function(){
-						return web3.eth.getBalance(contractAddress);
-					}).then(function(balance){
-						lotteryBalanceEnd = balance;
-						assert.equal(lotteryBalanceFirst + parseInt(web3.toWei(2,"finney")), lotteryBalanceEnd, "lottery balance should be 2 finney more than the lottery balance before a quarter ticket sold");							
-					});
-	});*/
+contract('Lottery', function(accounts){
 
+    it("a value not equal to 8 finney should revert the full ticket purchase", function(){
+        var ticketHash = web3.sha3(number1,number2,number3,accounts[0]);
+        var number1 = 1;
+        var number2 = 2;
+        var number3 = 3;
+
+        return Lottery.deployed().then(function(contract){
+            return contract.buyfullticket(ticketHash,{from:accounts[0],value:web3.toWei(7,"finney")});
+        }).catch(function(error){
+            if(error.toString().indexOf("revert") != -1){
+                console.log("Passed full ticket purchase revert test");
+            }
+            else{
+                assert(false,true,"it should revert full ticket purchase when a sender sends not equal to 8 finney.");
+            }
+        })
+
+    });
+
+});
+
+
+contract('Lottery', function(accounts){
+
+    it("exactly 4 finney should be send to buy a half ticket", function(){
+        var ticketHash = web3.sha3(number1,number2,number3,accounts[0]);
+        var number1 = 1;
+        var number2 = 2;
+        var number3 = 3;
+        var lotteryBalanceFirst;
+        var lotteryBalanceEnd;
+        var instance;
+
+        return Lottery.deployed().then(function(contract){
+            instance = contract;
+            return contract.getcontractbalance.call();
+        }).then(function(balance){
+            lotteryBalanceFirst = parseInt(balance);
+            return instance.buyhalfticket(ticketHash,{from:accounts[0],value:web3.toWei(4,"finney")});
+        }).then(function(){
+            return instance.getcontractbalance.call();
+        }).then(function(balance){
+            lotteryBalanceEnd = parseInt(balance);
+            assert.equal(lotteryBalanceFirst + parseInt(web3.toWei(4,"finney")),lotteryBalanceEnd,"lottery balance should be 4 finneys more than the lottery balance before a half ticket sell");
+        })
+
+    });
+
+});
+
+contract('Lottery', function(accounts){
+
+    it("a value not equal to 4 finney should revert the half ticket purchase", function(){
+        var ticketHash = web3.sha3(number1,number2,number3,accounts[0]);
+        var number1 = 1;
+        var number2 = 2;
+        var number3 = 3;
+
+        return Lottery.deployed().then(function(contract){
+            return contract.buyfullticket(ticketHash,{from:accounts[0],value:web3.toWei(3,"finney")});
+        }).catch(function(error){
+            if(error.toString().indexOf("revert") != -1){
+                console.log("Passed full ticket purchase revert test");
+            }
+            else{
+                assert(false,true,"it should revert half ticket purchase when a sender sends not equal to 4 finney.");
+            }
+        })
+
+    });
+
+});
+
+
+contract('Lottery', function(accounts){
+
+    it("exactly 2 finney should be send to buy a quarter ticket", function(){
+        var ticketHash = web3.sha3(number1,number2,number3,accounts[0]);
+        var number1 = 1;
+        var number2 = 2;
+        var number3 = 3;
+        var lotteryBalanceFirst;
+        var lotteryBalanceEnd;
+        var instance;
+
+        return Lottery.deployed().then(function(contract){
+            instance = contract;
+            return contract.getcontractbalance.call();
+        }).then(function(balance){
+            lotteryBalanceFirst = parseInt(balance);
+            return instance.buyquarterticket(ticketHash,{from:accounts[0],value:web3.toWei(2,"finney")});
+        }).then(function(){
+            return instance.getcontractbalance.call();
+        }).then(function(balance){
+            lotteryBalanceEnd = parseInt(balance);
+            assert.equal(lotteryBalanceFirst + parseInt(web3.toWei(2,"finney")),lotteryBalanceEnd,"lottery balance should be 2 finneys more than the lottery balance before a quarter ticket sell");
+        })
+
+    });
+
+});
+
+contract('Lottery', function(accounts){
+
+    it("a value not equal to 2 finney should revert the half ticket purchase", function(){
+        var ticketHash = web3.sha3(number1,number2,number3,accounts[0]);
+        var number1 = 1;
+        var number2 = 2;
+        var number3 = 3;
+
+        return Lottery.deployed().then(function(contract){
+            return contract.buyfullticket(ticketHash,{from:accounts[0],value:web3.toWei(1,"finney")});
+        }).catch(function(error){
+            if(error.toString().indexOf("revert") != -1){
+                console.log("Passed full ticket purchase revert test");
+            }
+            else{
+                assert(false,true,"it should revert quarter ticket purchase when a sender sends not equal to 2 finney.");
+            }
+        })
+
+    });
 
 });
 
